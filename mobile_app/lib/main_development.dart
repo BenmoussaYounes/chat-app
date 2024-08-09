129267c336c60a26c9bdb3d1cdda52b85985bd4f
younes <devbenmoussayounes@gmail.com>
2024-08-09 20:56:11 +0100

    setup(android-ios) : - setup firebase for android app both dev and production flavors - setup firebase for ios app - add cloud_firestore  package to enable interaction with firestore - refactor android/ios app id from com.attraxia.chat_app to com.attraxia.chatapp since it was not a valid bundle id for firebase

--- mobile_app/lib/main_development.dart
+++ mobile_app/lib/main_development.dart
@@ -1,8 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 import 'chat_app.dart';
 import 'core/routing/app_router.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+
   runApp(ChatApp(appRouter: AppRouter()));
 }
